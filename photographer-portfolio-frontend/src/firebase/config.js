91a2efb38ea164ec5ad7f0cfa64fd8ae23cddde5
voinar <38013623+voinar@users.noreply.github.com>
2022-12-22 15:27:14 +0100

    fix

---
+++ photographer-portfolio-frontend/src/firebase/config.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getFirestore, collection } from 'firebase/firestore';
+
+// dev
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'kacper-foto.firebaseapp.com',
+  projectId: 'kacper-foto',
+  storageBucket: 'kacper-foto.appspot.com',
+  messagingSenderId: '950070737414',
+  appId: '1:950070737414:web:232fc51cb42460e45fa187',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+
+// initiate storage
+const storage = getStorage(app);
+
+// initiate database
+const db = getFirestore();
+
+// refs
+const usersColRef = collection(db, 'users');
+const settingsColRef = collection(db, 'settings');
+const ordersColRef = collection(db, 'orders');
+
+export {
+  storage, db, usersColRef, settingsColRef, ordersColRef,
+};
